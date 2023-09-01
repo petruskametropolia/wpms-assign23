2653b8f0ef48bcb561b040d308bf4cb49f709464
petruskaj <136715032+petruskaj@users.noreply.github.com>
2023-09-02 00:13:37 +0300

    first-app

--- wpms-assign23/my-app/App.js
+++ wpms-assign23/my-app/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Platform} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import List from './components/List';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <List />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ wpms-assign23/my-app/components/ListItem.js
@@ -0,0 +1,25 @@
+import {Image, Text, TouchableOpacity} from 'react-native';
+import PropTypes from 'prop-types';
+
+const ListItem = ({singleMedia}) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        console.log('touched!', singleMedia.title);
+      }}
+    >
+      <Image
+        style={{width: 100, height: 100}}
+        source={{uri: singleMedia.thumbnails.w160}}
+      />
+      <Text>{singleMedia.title}</Text>
+      <Text>{singleMedia.description}</Text>
+    </TouchableOpacity>
+  );
+};
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object,
+};
+
+export default ListItem;
