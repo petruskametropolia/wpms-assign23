a1c6850525c027b3ba18fd04d40c1eae33445a11
petruskaj <136715032+petruskaj@users.noreply.github.com>
2023-09-05 05:17:51 +0300

    hooks

--- wpms-assign23/my-app/components/List.js
+++ wpms-assign23/my-app/components/List.js
@@ -1,33 +1,9 @@
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
-import {useEffect, useState} from 'react';
-
-import {apiUrl} from '../utils/app-config';
+import {useMedia} from '../hooks/ApiHooks';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async () => {
-    try {
-      const response = await fetch(apiUrl + 'media');
-      const json = await response.json();
-      
-      const mediaFiles = await Promise.all(
-        json.map(async (item) => {
-          const response = await fetch(apiUrl + 'media/' + item.file_id);
-          const fileData = await response.json();
-          return fileData;
-        }),
-      );
-      setMediaArray(mediaFiles);
-    } catch (error) {
-      console.error('loadMedia failed', error);
-    }
-  };
-
-  useEffect(() => {
-    loadMedia();
-  }, []);
+  const {mediaArray} = useMedia();
 
   return (
     <FlatList
---
+++ wpms-assign23/my-app/hooks/ApiHooks.js
@@ -0,0 +1,31 @@
+import {useEffect, useState} from 'react';
+import {apiUrl} from '../utils/app-config';
+import {doFetch} from '../utils/functions';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const json = await doFetch(apiUrl + 'media');
+
+      const mediaFiles = await Promise.all(
+        json.map(async (item) => {
+          const fileData = await doFetch(apiUrl + 'media/' + item.file_id);
+          return fileData;
+        }),
+      );
+      setMediaArray(mediaFiles);
+    } catch (error) {
+      console.error('loadMedia failed', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+  return {mediaArray};
+};
+
+export {useMedia};
